727bc8abfff758ee202a374c7d38031711f9d1a9
pelleranogiovanni <pelleranogiovanni@hotmail.com>
2022-07-20 18:14:13 -0300

    test

--- controllers/controllerLocation.js
+++ controllers/controllerLocation.js
@@ -4,8 +4,8 @@ const getLocation = async (req, res) => {
     try {
         let ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
         
-        const location = await service.getLocationWithIp(ip);
-        res.json(location);
+        res.json(ip);
+        // const location = await service.getLocationWithIp(ip);
         // res.json({
         //     status: location.status,
         //     data: location,
